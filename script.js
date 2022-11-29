6ca693577b1cad9accc94b28969b806b806437e7
jrolls81 <105545633+jrolls81@users.noreply.github.com>
2022-11-29 06:27:00 -0600

    completed studio with bonus missions

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 // TODO: add code here
+window.addEventListener("load", function(){
+
+    const container = document.getElementById('container');
+    const count = document.getElementById('count');
+
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(data){
+            console.log(data)
+        
+            data.sort(function(a, b){
+                return a.hoursInSpace < b.hoursInSpace ? 1 : -1;
+            });
+             
+            count.innerHTML = `There are ${data.length} astronauts!`
+
+            for (let i=0; i<data.length; i++){
+                let astronaut = data[i];
+                container.innerHTML += 
+                `<div class="astronaut">
+                    <div class="bio">
+                    <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${astronaut.hoursInSpace} </li>
+                        <li class="${astronaut.active ? "green" : ""}":> ${astronaut.active}</li>
+                        <li>Skills: ${astronaut.skills.join(', ')}r</li>
+                    </ul>
+                    </div>
+                    <img class="avatar" src=${astronaut.picture}>
+                </div>`
+            };
+        });
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    });
+    
